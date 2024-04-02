0f0ba68b51af5f07f296c40dfc997e2a45de3523
feng212 <2120086223@qq.com>
2024-04-02 16:22:18 +0800

    init

--- pkg/lustre-driver/controllerserver.go
+++ pkg/lustre-driver/controllerserver.go
@@ -1,1 +1,109 @@
+/*
+Copyright 2019 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package lustre_driver
+
+import (
+	"context"
+	"github.com/container-storage-interface/spec/lib/go/csi"
+)
+
+var (
+	// volumeCaps represents how the volume could be accessed.
+	volumeCaps = []csi.VolumeCapability_AccessMode{
+		{
+			Mode: csi.VolumeCapability_AccessMode_SINGLE_NODE_WRITER,
+		},
+		{
+			Mode: csi.VolumeCapability_AccessMode_MULTI_NODE_MULTI_WRITER,
+		},
+	}
+
+	// controllerCaps represents the capability of controller service
+	controllerCaps = []csi.ControllerServiceCapability_RPC_Type{
+		csi.ControllerServiceCapability_RPC_CREATE_DELETE_VOLUME,
+		csi.ControllerServiceCapability_RPC_EXPAND_VOLUME,
+	}
+)
+
+const (
+	volumeContextDnsName                      = "dnsname"
+	volumeContextMountName                    = "mountname"
+	volumeParamsSubnetId                      = "subnetId"
+	volumeParamsSecurityGroupIds              = "securityGroupIds"
+	volumeParamsAutoImportPolicy              = "autoImportPolicy"
+	volumeParamsS3ImportPath                  = "s3ImportPath"
+	volumeParamsS3ExportPath                  = "s3ExportPath"
+	volumeParamsDeploymentType                = "deploymentType"
+	volumeParamsKmsKeyId                      = "kmsKeyId"
+	volumeParamsPerUnitStorageThroughput      = "perUnitStorageThroughput"
+	volumeParamsStorageType                   = "storageType"
+	volumeParamsDriveCacheType                = "driveCacheType"
+	volumeParamsAutomaticBackupRetentionDays  = "automaticBackupRetentionDays"
+	volumeParamsDailyAutomaticBackupStartTime = "dailyAutomaticBackupStartTime"
+	volumeParamsCopyTagsToBackups             = "copyTagsToBackups"
+	volumeParamsDataCompressionType           = "dataCompressionType"
+	volumeParamsWeeklyMaintenanceStartTime    = "weeklyMaintenanceStartTime"
+	volumeParamsFileSystemTypeVersion         = "fileSystemTypeVersion"
+	volumeParamsExtraTags                     = "extraTags"
+)
+
+// controllerService represents the controller service of CSI driver
+type controllerService struct {
+}
+
+func (cs *controllerService) CreateVolume(ctx context.Context, req *csi.CreateVolumeRequest) (*csi.CreateVolumeResponse, error) {
+	return nil, nil
+}
+func (cs *controllerService) DeleteVolume(ctx context.Context, req *csi.DeleteVolumeRequest) (*csi.DeleteVolumeResponse, error) {
+	return nil, nil
+}
+func (cs *controllerService) ControllerPublishVolume(ctx context.Context, req *csi.ControllerPublishVolumeRequest) (*csi.ControllerPublishVolumeResponse, error) {
+	return nil, nil
+}
+func (cs *controllerService) ControllerUnpublishVolume(ctx context.Context, req *csi.ControllerUnpublishVolumeRequest) (*csi.ControllerUnpublishVolumeResponse, error) {
+	return nil, nil
+}
+func (cs *controllerService) ValidateVolumeCapabilities(ctx context.Context, req *csi.ValidateVolumeCapabilitiesRequest) (*csi.ValidateVolumeCapabilitiesResponse, error) {
+	return nil, nil
+}
+func (cs *controllerService) ListVolumes(ctx context.Context, req *csi.ListVolumesRequest) (*csi.ListVolumesResponse, error) {
+	return nil, nil
+}
+func (cs *controllerService) GetCapacity(ctx context.Context, req *csi.GetCapacityRequest) (*csi.GetCapacityResponse, error) {
+	return nil, nil
+}
+func (cs *controllerService) ControllerGetCapabilities(ctx context.Context, req *csi.ControllerGetCapabilitiesRequest) (*csi.ControllerGetCapabilitiesResponse, error) {
+	return nil, nil
+}
+func (cs *controllerService) CreateSnapshot(ctx context.Context, req *csi.CreateSnapshotRequest) (*csi.CreateSnapshotResponse, error) {
+	return nil, nil
+}
+func (cs *controllerService) DeleteSnapshot(ctx context.Context, req *csi.DeleteSnapshotRequest) (*csi.DeleteSnapshotResponse, error) {
+	return nil, nil
+}
+func (cs *controllerService) ListSnapshots(ctx context.Context, req *csi.ListSnapshotsRequest) (*csi.ListSnapshotsResponse, error) {
+	return nil, nil
+}
+func (cs *controllerService) ControllerExpandVolume(ctx context.Context, req *csi.ControllerExpandVolumeRequest) (*csi.ControllerExpandVolumeResponse, error) {
+	return nil, nil
+}
+func (cs *controllerService) ControllerGetVolume(ctx context.Context, req *csi.ControllerGetVolumeRequest) (*csi.ControllerGetVolumeResponse, error) {
+	return nil, nil
+}
+func (cs *controllerService) ControllerModifyVolume(ctx context.Context, req *csi.ControllerModifyVolumeRequest) (*csi.ControllerModifyVolumeResponse, error) {
+	return nil, nil
+}
--- pkg/lustre-driver/nodeserver.go
+++ pkg/lustre-driver/nodeserver.go
@@ -1,1 +1,34 @@
 package lustre_driver
+
+import (
+	"context"
+	"github.com/container-storage-interface/spec/lib/go/csi"
+)
+
+type nodeService struct {
+}
+
+func (ns *nodeService) NodeStageVolume(ctx context.Context, req *csi.NodeStageVolumeRequest) (*csi.NodeStageVolumeResponse, error) {
+	return nil, nil
+}
+func (ns *nodeService) NodeUnstageVolume(ctx context.Context, req *csi.NodeUnstageVolumeRequest) (*csi.NodeUnstageVolumeResponse, error) {
+	return nil, nil
+}
+func (ns *nodeService) NodePublishVolume(ctx context.Context, req *csi.NodePublishVolumeRequest) (*csi.NodePublishVolumeResponse, error) {
+	return nil, nil
+}
+func (ns *nodeService) NodeUnpublishVolume(ctx context.Context, req *csi.NodeUnpublishVolumeRequest) (*csi.NodeUnpublishVolumeResponse, error) {
+	return nil, nil
+}
+func (ns *nodeService) NodeGetVolumeStats(ctx context.Context, req *csi.NodeGetVolumeStatsRequest) (*csi.NodeGetVolumeStatsResponse, error) {
+	return nil, nil
+}
+func (ns *nodeService) NodeExpandVolume(ctx context.Context, req *csi.NodeExpandVolumeRequest) (*csi.NodeExpandVolumeResponse, error) {
+	return nil, nil
+}
+func (ns *nodeService) NodeGetCapabilities(ctx context.Context, req *csi.NodeGetCapabilitiesRequest) (*csi.NodeGetCapabilitiesResponse, error) {
+	return nil, nil
+}
+func (ns *nodeService) NodeGetInfo(ctx context.Context, req *csi.NodeGetInfoRequest) (*csi.NodeGetInfoResponse, error) {
+	return nil, nil
+}
